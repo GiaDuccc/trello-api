f244806fe48aff513b7ffb65d17a09b61832510f
Gia Duc <giaduc19042004@gmail.com>
2025-03-10 22:09:04 +0700

    Init Code

---
+++ src/server.js
@@ -0,0 +1,14 @@
+// const express = require('express'); 
+import express from 'express';
+
+const app = express();
+const hostName = 'localhost';
+const port = 8017;
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello Gia Duc</h1>')
+});
+
+app.listen(port, hostName, () => {
+  console.log(`running http://${hostName}:${port}/`);
+});
